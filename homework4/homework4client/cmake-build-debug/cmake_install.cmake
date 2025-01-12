bd18f909689ea88b0178d3635b6e660cf8fceac9
“Dmytro“ <maslovsky2005@gmail.com>
2025-01-12 19:36:17 +0200

    Add server and client code

--- homework4/homework4client/cmake-build-debug/cmake_install.cmake
+++ homework4/homework4client/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/Dima/CLionProjects/homework4client
+# Install script for directory: C:/Users/Dima/Desktop/trspo/homework4/homework4client
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/Dima/CLionProjects/homework4client/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/Dima/Desktop/trspo/homework4/homework4client/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
